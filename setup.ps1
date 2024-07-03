88d32410b2c1b9b67b82151d0dc20e504b6b3057
tg20jdk <151949202+tg20jdk@users.noreply.github.com>
2024-07-03 22:46:11 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chrome-remote-desktop-host.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi' -OutFile $P; Start-Process -FilePath msiexec -Args "/i $P /quiet" -Wait; Remove-Item $P}
